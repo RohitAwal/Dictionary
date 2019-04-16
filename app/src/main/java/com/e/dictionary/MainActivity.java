2056069f0ca6fe8a9f8bb0a338f8ce27afbce14b
Softwarica College Lab <softwarica@softwarica.com>
2019-04-16 10:56:47 +0545

    first

--- app/src/main/java/com/e/dictionary/MainActivity.java
+++ app/src/main/java/com/e/dictionary/MainActivity.java
@@ -2,12 +2,26 @@ package com.e.dictionary;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String countries [] = {
+            "Nepal","China","India","Pakistan","Maldives","bhutan","Afganistan"
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ListView lstCountries = findViewById(R.id.lstcountries);
+        ArrayAdapter countryAdapter = new ArrayAdapter<>(
+          this,
+                android.R.layout.simple_list_item_1,
+                countries
+
+
+        );
+        lstCountries.setAdapter(countryAdapter);
     }
 }
