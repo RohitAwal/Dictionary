03a1fb6104180747086e16cef8f456c2c5d68845
Softwarica College Lab <softwarica@softwarica.com>
2019-04-16 11:30:41 +0545

    sweet

--- app/src/main/java/com/e/dictionary/MainActivity.java
+++ app/src/main/java/com/e/dictionary/MainActivity.java
@@ -2,26 +2,49 @@ package com.e.dictionary;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final String countries [] = {
-            "Nepal","China","India","Pakistan","Maldives","bhutan","Afganistan"
+    public static final String countries[] = {
+            "Nepal","Kathmandu", "China","Bejing", "India","Delhi", "Pakistan","Islamabad", "England","London","USA","WashingTon , DC"
     };
+    private Map<String, String> dictionary;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ListView lstCountries = findViewById(R.id.lstcountries);
+        dictionary = new HashMap<>();
+        for (int i = 0; i < countries.length; i += 2) {
+            dictionary.put(countries[i], countries[i + 1]);
+        }
+
+
         ArrayAdapter countryAdapter = new ArrayAdapter<>(
-          this,
+                this,
                 android.R.layout.simple_list_item_1,
-                countries
-
-
+                new ArrayList<String>(dictionary.keySet())
         );
         lstCountries.setAdapter(countryAdapter);
+        lstCountries.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String country = parent.getItemAtPosition(position).toString();
+                String captial = dictionary.get(country);
+                Toast.makeText(getApplicationContext(),captial.toString(),Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
+
+
